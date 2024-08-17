0fa6190cc2d50946210f154081bdf8f2f8221c40
pushpakninave <pushpakninave.jee@gmail.com>
2024-08-17 14:15:23 +0530

    did some tweaks

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ import { FiDownload } from 'react-icons/fi'
 export default function Home() {
   return (
     <section className="h-full">
-      <div className="container mx-auto h-full">
+      <div className="container pt-10 mx-auto h-full">
         <div className="mt-4 mx-2 md:mx-10 xl:mx-10 lg:mx-10 flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:mb-7">
           {/* text */}
           <div className="text-start xl:text-left order-2 xl:order-none">
@@ -22,6 +22,7 @@ export default function Home() {
               I’m a software engineer specializing in building applications. Currently, I’m focused on building accessible, human-centered products at a SaaS firm.
             </p>
             {/* <Stats/> */}
+            {/* <MediaEmbed/> */}
             {/* button and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8 mt-20">
               <a href="/assets/dnfile/Pushpak_FResume.pdf" download="pushpakninave_resume.pdf">
@@ -34,7 +35,7 @@ export default function Home() {
                   <FiDownload className="text-xl" />
                 </Button>
               </a>
-              <div className="mb-8 mt-5 xl:mb-0">
+              <div className="mb-8 xl:mb-0">
                 <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
               </div>
             </div>
